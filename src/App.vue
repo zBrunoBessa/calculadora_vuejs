5037b0cfc5d674f47da8a74c48d53d0105af3fb9
Bruno Bessa <beduardo2014@gmail.com>
2023-04-02 09:46:44 -0300

    projeto final

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,78 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { reactive } from 'vue';
+  const estado = reactive ({
+    operacao: 'soma',
+    numero1: 0,
+    numero2: 0,
+  });
+  const operacoes = () => {
+      const {operacao} = estado;
+      switch (operacao){
+        case 'soma':
+          return parseFloat(estado.numero1) + parseFloat(estado.numero2)
+        case 'subtracao':
+          return parseFloat(estado.numero1) - parseFloat(estado.numero2);
+        case 'divisao':
+          return parseFloat(estado.numero1) / parseFloat(estado.numero2);
+        case 'multiplicacao':
+          return parseFloat(estado.numero1) * parseFloat(estado.numero2);  
+      }
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>Calculadora</h1>
+    <div>
+    <label>Informe o numero 1:</label>
+    <input type="number" required @keyup="evento => estado.numero1 = evento.target.value">
+  </div>
+  <div>
+    <label>Informe o numero 2:</label>
+    <input type="number" required @keyup="evento => estado.numero2 = evento.target.value">
+  </div>
+  <div>
+  <label>Operação:</label>
+    <select @change="evento => estado.operacao = evento.target.value">
+      <option value="soma">+</option>
+      <option value="subtracao">-</option>
+      <option value="divisao">/</option>
+      <option value="multiplicacao">x</option>
+    </select>
+  </div>
+    <span>Resultado : <span class="result">{{operacoes(0)}}</span> </span>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+  .container{
+    font-size: 24px;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    flex-direction: column;
+    max-height: 400px;
+    background-color: #eee;
+    padding: 45px;
+    align-items: center;
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
-}
+  h1 {
+    margin-bottom: 16px;
+
+  }
+  input {
+    margin-left: 8px;
+    width: 80px;
+  }
+  select {
+    margin-left: 12px;
+    width: 90px;
+
+  }
+  span {
+    margin: 10px;
+  }
+  .result {
+    color: #ff1111;
+  }
 </style>
